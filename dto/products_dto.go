ea5b714e3e4b56439b37d2fddef52051908bf3e1
Bluore <2260762800@qq.com>
2026-01-25 20:08:36 +0800

    ! 添加dto文件夹

---
+++ dto/products_dto.go
@@ -0,0 +1,56 @@
+package dto
+
+const (
+	ERRConditionInvalid = errors.New("成色参数无效")
+	ERRTradeTypeInvalid = errors.New("交易方式参数无效")
+	ERRTransactionMethod= errors.New("邮寄方式参数无效")
+)
+
+type ProductDTO struct {
+	CategoryID   int64   `json:"category_id" binding:"required"`    //分类ID
+	Title        string  `json:"title" binding:"required"`          //标题
+	IntroText    string  `json:"intro_text" binding:"required"`     //描述
+	OriginPrice  float64 `json:"origin_price" binding:"required"`   //原价（打折前）
+	MainImageUrl string  `json:"main_image_url" binding:"required"` //主图
+	Condition	string  `json:"condition" binding:"required"`      //成色
+	TradeType    int     `json:"trade_type" binding:"required"`     //交易方式
+	FreeShipping bool    `json:"free_shipping" binding:"required"`  //邮寄方式
+}
+
+func (p *ProductDTO) GetCondition() (string, error) {
+	switch p.Condition {
+	case "全新":
+		return "全新", nil
+	case "几乎全新":
+		return "几乎全新", nil
+	case "九成新":
+		return "九成新", nil
+	case "八成新":
+		return "八成新", nil
+	case "有破损"
+		return "有破损", nil
+	default:
+		return "", ERRConditionInvalid
+	}
+}
+
+func (p *ProductDTO) GetTradeType() (string, error) {
+	switch p.Condition {
+	case "快递":
+		return "快递", nil
+	case "自提":
+		return "自提", nil
+	default:
+		return "", ERRTradeTypeInvalidd
+	}
+}
+
+func (p *ProductDTO) GetTransactionMethod() (string, error) {
+	switch p.Condition {
+	// todo 补充交易方式
+
+
+	default:
+		return "", ERRTransactionMethod
+	}
+}
