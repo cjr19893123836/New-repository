5ae088d944cb6bd8a20ebf6cd0cea46264c4e741
1Huangqin <1668149397@qq.com>
2026-01-25 19:17:48 +0800

    Product_Details Second

--- http_models/category.go
+++ http_models/category.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 // Category 商品类别表模型
 type Category struct {
-	gorm.Association
+	gorm.Model
 	Image     string `gorm:"column:image;size:1024"`      //分类图
 	SortOrder int    `gorm:"column:sort_order;default:0"` //排序
 	TagName   string `gorm:"column:tag_name;"`            //分类名称
